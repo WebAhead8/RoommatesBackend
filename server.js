ed1ce0aa7f267fc142ee659814fc16a31ca7d128
khaled ba <badarnakhaled@gmail.com>
2021-03-19 16:32:41 +0200

    router

--- server.js
+++ server.js
@@ -2,10 +2,37 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("<h1>Hiiidfftgtgsdcdcsdfrfctgbgbdii</h1>");
+  res.send("<h1>home</h1>");
 });
+server.get("/allposts", (req, res) => {
+  res.send("<h1>allposts</h1>");
+});
+server.get("/myporfile", (req, res) => {
+  res.send("<h1>myporfile</h1>");
+});
+server.get("/allprofiles", (req, res) => {
+  res.send("<h1>allprofiles</h1>");
+});
+
 server.get("/aboutus", (req, res) => {
-  res.send("<h1>Hiedetgsdcdcffsdfrfctgbgbdii</h1>");
+  res.send("<h1>aboutus</h1>");
+});
+server.get("/signup", (req, res) => {
+  res.send("<h1>signup</h1>");
+});
+
+server.get("/signup2", (req, res) => {
+  res.send("<h1>signup2</h1>");
+});
+server.get("/login", (req, res) => {
+  res.send("<h1>login</h1>");
+});
+
+server.get("/chat", (req, res) => {
+  res.send("<h1>chat</h1>");
+});
+server.get("/findroommates", (req, res) => {
+  res.send("<h1>findroommates</h1>");
 });
 
 server.listen(3000, () =>
