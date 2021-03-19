90450293d8dde0e196acb691b8142c550f974bf5
MayDarawshi <May.Darawshi19@gmail.com>
2021-03-19 16:19:29 +0200

    about

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const server = express();
+
+server.get("/", (req, res) => {
+  res.send("<h1>Hiiidfftgtgsdcdcsdfrfctgbgbdii</h1>");
+});
+server.get("/aboutus", (req, res) => {
+  res.send("<h1>Hiedetgsdcdcffsdfrfctgbgbdii</h1>");
+});
+
 server.listen(3000, () =>
   console.log("Server listening on http:localhost:3000")
 );
