9aad53fe1f7cc61455f94ce1d099ce50068704f2
MayDarawshi <May.Darawshi19@gmail.com>
2021-03-21 09:11:07 +0200

    updated

--- model/users.js
+++ model/users.js
@@ -9,7 +9,18 @@ function getAllUser() {
 }
 
 function createUser(user) {
-  const values = [user.username, user.pass, user.email,user.gender,user.age,user.currentloc,user.moveto,user.university,user.studing,user.pic];
+  const values = [
+    user.username,
+    user.pass,
+    user.email,
+    user.gender,
+    user.age,
+    user.currentloc,
+    user.moveto,
+    user.university,
+    user.studing,
+    user.pic,
+  ];
   return db
     .query(
       "INSERT INTO users(user.username, user.pass, user.email,user.gender,user.age,user.currentloc,user.moveto,user.university,user.studing,user.pic) VALUES($1, $2, $3,$4,$5,$6,$7,$8,$9,$10)  returning *",
@@ -18,4 +29,30 @@ function createUser(user) {
     .then((result) => {
       return result.rows;
     });
+}
+function getUser(email) {
+  const values = [email];
+  return db.query("select * from users where email=$1", values).then((data) => {
+    if (!data.rows.length)
+      throw new Error(`No user with email '${email}' found`);
+    return data.rows[0];
+  });
+}
+function getUser(username) {
+  const values = [username];
+  return db
+    .query("select * from users where username=$1", values)
+    .then((data) => {
+      if (!data.rows.length)
+        throw new Error(`No user with username '${username}' found`);
+      return data.rows[0];
+    });
+}
+
+function getUserById(id) {
+  const values = [id];
+  return db.query("select * from users where id=$1", values).then((data) => {
+    if (!data.rows.length) throw new Error(`No user with id '${id}' found`);
+    return data.rows[0];
+  });
 }
